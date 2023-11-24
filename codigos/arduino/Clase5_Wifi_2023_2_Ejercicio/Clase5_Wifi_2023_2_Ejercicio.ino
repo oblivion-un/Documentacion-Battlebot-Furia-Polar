b2432d067f21abc2ffbebd8027c9d5e6cbb982a8
S-Avalloni <104738448+S-Avalloni@users.noreply.github.com>
2023-11-24 18:31:38 -0300

    Update Clase5_Wifi_2023_2_Ejercicio.ino

--- codigos/arduino/Clase5_Wifi_2023_2_Ejercicio/Clase5_Wifi_2023_2_Ejercicio.ino
+++ codigos/arduino/Clase5_Wifi_2023_2_Ejercicio/Clase5_Wifi_2023_2_Ejercicio.ino
@@ -1,10 +1,3 @@
-/** Clase 5 Control WiFi 
-  @name Clase5_Wifi_2023-2
-  @purpose: En esta clase aprenderán a usar la funcionalidad wifi de la ESP8266
-  @author: Marcelo Jimenez
-  @version: Esta corresponde al ejercicio de la clase práctica.  
-*/
-
 #include "motores.h"
 #include "web_server.h"
 
